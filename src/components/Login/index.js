baa621e026abe837bc11efd4838d11f47ec96ddd
Jherod Brendon <jherodjb@gmail.com>
2019-11-04 12:00:07 -0300

    App OK

---
+++ src/components/Login/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Container, Header } from "semantic-ui-react";
+
+class LoginPage extends Component {
+  render() {
+    return (
+      <Container fluid>
+        <Header>Gentilezas</Header>
+      </Container>
+    );
+  }
+}
+
+export default LoginPage;
